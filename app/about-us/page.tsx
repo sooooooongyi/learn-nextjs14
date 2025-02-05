748d7ea464dbfb31f92c3daa21b37312116f6927
sooooooongyi <sooooooongyi@gmail.com>
2025-02-05 23:31:37 +0900

    feat: layout

--- app/about-us/page.tsx
+++ app/about-us/page.tsx
@@ -1,9 +1,6 @@
-import Navigation from "../../components/Navigation";
-
 export default function AboutUs() {
   return (
     <div>
-      <Navigation />
       <h1>About Us!</h1>
     </div>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,6 @@
-import Navigation from "../components/Navigation";
-
 export default function Tomate() {
   return (
     <div>
-      <Navigation />
       <h1>Hello!</h1>
     </div>
   );
