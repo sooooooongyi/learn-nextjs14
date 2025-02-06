9a374ff420a046474c93549eaeee017a3b8edf10
sooooooongyi <sooooooongyi@gmail.com>
2025-02-06 18:17:13 +0900

    feat: movie detail style

--- components/movie-info.tsx
+++ components/movie-info.tsx
@@ -1,4 +1,5 @@
 import { API_URL } from "../app/(home)/page";
+import potato from "../styles/movie-info.module.css";
 
 async function getMovie(id: string) {
   const response = await fetch(`${API_URL}/${id}`);
@@ -6,6 +7,22 @@ async function getMovie(id: string) {
 }
 
 export default async function MovieInfo({ id }: { id: string }) {
-  const videos = await getMovie(id);
-  return <h6>{JSON.stringify(videos)}</h6>;
+  const movie = await getMovie(id);
+  return (
+    <div className={potato.container}>
+      <img
+        src={movie.poster_path}
+        className={potato.poster}
+        alt={movie.title}
+      />
+      <div className={potato.info}>
+        <h1 className={potato.title}>{movie.title}</h1>
+        <h3>⭐️ {movie.vote_average.toFixed(1)}</h3>
+        <p>{movie.overview}</p>
+        <a href={movie.homepage} target={"_blank"}>
+          Homepage &rarr;
+        </a>
+      </div>
+    </div>
+  );
 }
